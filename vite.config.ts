aba0b4badb1e8017ba64d2877d0d489368b279bd
abdullahrehman0069-droid <abdullahrehman0069@gmail.com>
2025-12-22 04:24:02 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
-      // ADD THIS LINE HERE:
-      base: "/eco-wave/", 
+
+      base: "/Eco-Wave/", 
 
       server: {
         port: 3000,
@@ -14,8 +14,11 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        // This fixes the "process is not defined" crash
+        'process.env': {
+           API_KEY: env.GEMINI_API_KEY,
+           GEMINI_API_KEY: env.GEMINI_API_KEY
+        }
       },
       resolve: {
         alias: {
